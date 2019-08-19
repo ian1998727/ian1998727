973cb5ee48ec01f015e015506f43a51a5cb8676d
ian1998727 <53325820+ian1998727@users.noreply.github.com>
2019-08-19 15:09:53 +0800

    Add files via upload

---
+++ asyncDB.js
@@ -0,0 +1,41 @@
+'use strict';
+
+//-----------------------
+// 引用資料庫模組
+//-----------------------
+const {Client} = require('pg');
+
+//-----------------------
+// 自己的資料庫連結位址
+//-----------------------
+var pgConn = 'postgres://mmopgvcydqshfc:7396c6f1ba66851eb89182c71e5509028d66e81b07f44e055ad22877d9acb89e@ec2-174-129-231-116.compute-1.amazonaws.com:5432/df3btm29r94oaq';
+
+
+//產生可同步執行query物件的函式
+function query(sql, value=null) {
+    return new Promise((resolve, reject) => {
+        //設定資料庫連線物件
+        var client = new Client({
+            connectionString: pgConn,
+            ssl: true
+        })     
+
+        //連結資料庫
+        client.connect();
+
+        //回覆查詢結果  
+        client.query(sql, value, (err, results) => {                   
+            if (err){
+                reject(err);
+            }else{
+                resolve(results);
+            }
+
+            //關閉連線
+            client.end();
+        });
+    });
+}
+
+//匯出
+module.exports = query;
